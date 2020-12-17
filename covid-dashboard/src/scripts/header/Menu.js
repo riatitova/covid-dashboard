8ebccb8f044b8f488f0241275af8e583a8071979
Tasty <sapegin.petya@yandex.ru>
2020-12-17 18:58:56 +0400

    feat: create header

---
+++ covid-dashboard/src/scripts/header/Menu.js
@@ -0,0 +1,23 @@
+import createDOMElement from '../createDOMElement';
+
+export default class Menu {
+  constructor(parent) {
+    this.parent = parent || null;
+    this.data = {
+      elementName: 'div',
+      classNames: 'header__menu',
+      children: null,
+      parent: this.parent,
+    };
+
+    this.renderMenu();
+  }
+
+  renderMenu() {
+    this.menuElement = createDOMElement(this.data);
+  }
+
+  toggleMenu() {
+    this.menuElement.classList.toggle('header__menu_visible');
+  }
+}
